13fb37d76c00bf532f4643864fe33070997b2166
Charity Hickman <lynsy9@gmail.com>
2013-01-30 19:33:16 -0800

    Update Lab6.java
    Rough menu options.

--- Lab6.java
+++ Lab6.java
@@ -2,6 +2,31 @@ class Lab6
 {
 	public static void main(String [] args)	
 	{
-
+        while (true)
+        {
+        	System.out.println("Please select from the following options.");
+        	System.out.println("1. Guess my number.         2. TicTacToe.");
+           	System.out.println("3. Calculator.              4. Story")
+           	System.out.println("Enter any other value to exit.")
+        	int game = input.nextInt();
+        	switch(game)
+        	{
+        		case 1:
+        			Number.playerGuess();
+        			break;
+        		case 2:
+        			TicTacToe.method();
+        			break;
+        		case 3:
+        			Calculator.method();
+        			break;
+        		case 4:
+        			Story.method();
+        			break;
+        		default:
+        			System.out.println("Goodbye!");
+        			break;
+        	}
+        }
 	}
 }
