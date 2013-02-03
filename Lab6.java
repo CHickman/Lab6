748ad6dfe03be2572e17434c5447c87967ce5eb0
dmhagan77 <dahagan77@yahoo.com>
2013-02-03 14:49:26 -0800

    Update Lab6.java
    Took care of user error (non-integer value).  Done as far as Extra Credit is concerned.

--- Lab6.java
+++ Lab6.java
@@ -1,32 +1,66 @@
+import java.util.*;
 class Lab6
 {
+	static Scanner input = new Scanner(System.in);
+	static boolean menu = false;
+	
 	public static void main(String [] args)	
 	{
-        while (true)
-        {
-        	System.out.println("Please select from the following options.");
-        	System.out.println("1. Guess my number.         2. TicTacToe.");
-           	System.out.println("3. Calculator.              4. Story")
-           	System.out.println("Enter any other value to exit.")
-        	int game = input.nextInt();
-        	switch(game)
-        	{
-        		case 1:
-        			Number.playerGuess();
-        			break;
-        		case 2:
-        			TicTacToe.game();
-        			break;
-        		case 3:
-        			Calculator.calculate();
-        			break;
-        		case 4:
-        			MadLibs.Fill_In();
-        			break;
-        		default:
-        			System.out.println("Goodbye!");
-        			break;
-        	}
-        }
+		while (!menu) {
+			int game = getMenu();
+	        selectGame(game);
+	        if (!menu) {
+	        	while (useAgain(game)) {
+	        		selectGame(game);
+	        	}
+	        }
+		}
+	}
+	
+	public static int getMenu() {
+		{
+	       	System.out.println("Please select from the following options.");
+	    	System.out.println("1. Guess my number.         2. TicTacToe.");
+	       	System.out.println("3. Calculator.              4. Mad Lib");
+	       	System.out.print("Enter any other value to exit: ");         	
+	       	while(!input.hasNextInt())
+	       	{  		
+	       		input.next();
+	       		System.out.println("Please use an integer value");
+	       	}
+    		int game = input.nextInt();
+	       	return game;
+	    }
+	}
+	public static void selectGame(int game) {
+	switch(game)
+		{
+			case 1:
+				Number.playerGuess();
+				break;
+			case 2:
+				TicTacToe.ticTacToe();
+				break;
+			case 3:
+				Calculator.calculate();
+				break;
+			case 4:
+				MadLib.Fill_In();
+				break;
+			default:
+				System.out.println("Goodbye!");
+				menu = true;
+				break;
+		}
+	}
+	
+	public static boolean useAgain(int game) {
+		System.out.println("Want to use the app again? Enter Yes or No");
+		char useAgain = input.next().toUpperCase().charAt(0);
+		if (useAgain == 'Y') {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
