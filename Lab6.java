af4dbc28ca4e23e2c385e35239be0dca06975e81
Charity Hickman <lynsy9@gmail.com>
2013-02-03 12:42:21 -0800

    Update Lab6.java

--- Lab6.java
+++ Lab6.java
@@ -21,7 +21,7 @@ class Lab6
         			Calculator.calculate();
         			break;
         		case 4:
-        			Story.method();
+        			Madlibs.Fill_In();
         			break;
         		default:
         			System.out.println("Goodbye!");
